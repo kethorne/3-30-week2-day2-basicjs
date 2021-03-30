b93f75c1b3c8bc9b57c654b78e5fbe39f25b7a6d
Karin Thorne <kethorne17@gmail.com>
2021-03-29 23:27:52 -0400

    basic navbar layout, searchbar w/ button, image, form input

---
+++ main.js
@@ -0,0 +1,21 @@
+const myHeading = document.getElementById("myTitle");
+myHeading.textContent = "Coding Some Basic Javascript";
+
+function searchFunction() {
+    let result = document.getElementById("search").value;
+    console.log(result);
+}
+
+
+const form = document.getElementById("tigerPhoto");
+
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    let resultText = document.getElementById("tiger").value;
+    console.log(resultText)
+});
+
+// function photoText() {
+//     let resultText = document.getElementById("tiger").value;
+//     console.log(resultText);
+// }
